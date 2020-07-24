3a2a8b0355984e843b1a96b98dcd6ceed07c52d7
antonisierakowski <antonisierakowski@gmail.com>
2020-07-24 10:52:37 +0200

    WIP

--- src/components/EventForm/index.spec.tsx
+++ src/components/EventForm/index.spec.tsx
@@ -1,4 +1,35 @@
-// import React from 'react';
+import React from 'react';
+import { EventForm } from "./index";
+import { render, RenderResult, fireEvent } from '@testing-library/react';
+import { RootComponent } from "../../root";
+import { act } from "react-dom/test-utils";
+
+let renderResult: RenderResult;
+
+beforeEach(() => {
+  act(() => {
+    renderResult = render(
+      // @ts-ignore
+      <RootComponent>
+        <EventForm />
+      </RootComponent>
+    );
+  })
+})
+
+it('should match to generated snapshot', () => {
+  expect(renderResult.asFragment).toMatchSnapshot();
+});
+
+it('should validate all fields on submit click', () => {
+  act(() => {
+    const submitButton = renderResult.getByText('Submit')
+    fireEvent.click(submitButton);
+
+    expect
+  });
+});
+
 // all integration tests
 
 // click on date should open picker
